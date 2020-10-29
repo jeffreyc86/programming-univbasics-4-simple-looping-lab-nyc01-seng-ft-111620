64f4429dd5600cf2a0d466586a41373bd1eae7ff
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-10-29 22:39:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+
+def loop_message_five_times (message)
+  counter = 0
+  while counter < 5 do
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times (message, n)
+  counter = 0
+  while counter < n do
+    puts message
+    counter += 1
+  end
+end
+
+def out_array (array)
+  
